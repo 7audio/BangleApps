57c06490222c5c2b5028e2b7e3ef30daf95f868a
Tikhon Sokolov <tikhon.sokolov>
2023-01-23 18:53:45 +0700

    update

--- apps/widklok/widget.js
+++ apps/widklok/widget.js
@@ -1,14 +1,13 @@
-(() => {
-  function draw() {
-    var date = new Date();
-    var timeStr = require("locale").time(date, 1).split(":"); // Hour and minute
-    g.reset().setColor(g.theme.dark ? "#0ff" : "#00f").setFont("6x8", 1);
-    g.drawString(timeStr[0], this.x+2, this.y+4, true);
-    g.drawString(timeStr[1], this.x+2, this.y+14, true);
-  }
-  WIDGETS["widklok"] = { area:"tr", width:16, draw: draw };
+WIDGETS["widklok"] = { area:"tr", width:16, draw: function() {
+  var date = new Date();
+  var timeStr = require("locale").time(date, 1).split(":"); // Hour and minute
+  g.reset().setColor(g.theme.dark ? "#0ff" : "#00f").setFont("6x8", 1);
+  g.drawString(timeStr[0], this.x+2, this.y+4, true);
+  g.drawString(timeStr[1], this.x+2, this.y+14, true);
+} };
 
-  setInterval(function() {
-    WIDGETS["widklok"].draw(WIDGETS["widklok"]);
-  }, 20000);
-})();
+if (this.klokDroTimeout) clearTimeout(this.klokDroTimeout);
+this.klokDroTimeout = setTimeout(()=>{
+  this.klokDroTimeout = undefined;
+  this.draw();
+}, 60000 - (Date.now() % 60000));
