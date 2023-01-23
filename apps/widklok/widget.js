a79ebe5ec2c80c837c19a9e1d90ef19e40cb030f
Tikhon Sokolov <tikhon.sokolov>
2023-01-23 18:17:46 +0700

    add widklok

---
+++ apps/widklok/widget.js
@@ -0,0 +1,10 @@
+(() => {
+  function draw() {
+    var date = new Date();
+    var timeStr = require("locale").time(date, 1).split(":"); // Hour and minute
+    g.reset().setColor(g.theme.dark ? "#0ff" : "#00f").setFont("6x8", 1);
+    g.drawString(timeStr[0], this.x+2, this.y+4, true);
+    g.drawString(timeStr[1], this.x+2, this.y+14, true);
+  }
+  WIDGETS["widid"] = { area:"tr", width:16, draw: draw };
+})();
