8bb66841f9d5b1f0df3616c44c8bf1f1baf6de7b
Tikhon Sokolov <tikhon.sokolov>
2023-02-01 20:13:28 +0700

    add pm2.5 bangkok

--- apps/a_clock_timer/app.js
+++ apps/a_clock_timer/app.js
@@ -17,10 +17,20 @@ function showWelcomeMessage() {
   g.reset().clearRect(0, 76, 44+44, g.getHeight()/2+6);
   g.setFontAlign(0, 0).setFont("6x8");
   Bangle.http("https://api.waqi.info/feed/Bangkok/?token=baf1c562ecda746eff772626b2ed246e8a66ffa3").then(data => {
+    var aqiStatuses = `0-50	Good
+51-100\tModerate
+101-150\tUnhealthy for Sensitive Groups
+151-200\tUnhealthy
+201-300\tVery Unhealthy
+300-999999\tHazardous`;
+    var readableStatus = aqiStatuses.split("\n").find(statusLine => {
+      var aqiRange = statusLine.split("\t")[0].split("-");
+      return data.data.aqi >= aqiRange[0] && data.data.aqi <= aqiRange[1];
+    }) || '0\tUnknown';
     var data = JSON.parse(data.resp);
     g.setFont('6x8').setFontAlign(0, 0);
-    g.drawString(`AQI: ${data.data.aqi}`, 44, 70);
-    g.drawString(`PM2.5: ${data.data.iaqi.pm25.v}`, 44, 80);
+    g.drawString(readableStatus.split("\t")[1], 44, 80);
+    g.drawString(`PM2.5: ${data.data.iaqi.pm25.v}`, 44, 70);
   }).catch((err) => {
     g.drawString('err', 44, 80);
   });
