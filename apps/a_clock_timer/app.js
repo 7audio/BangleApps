c12b0d15892116f48d8e8a2ad0c2604dededbb22
Tikhon Sokolov <tikhon.sokolov>
2023-02-01 22:46:17 +0700

    add pm2.5 bangkok

--- apps/a_clock_timer/app.js
+++ apps/a_clock_timer/app.js
@@ -69,6 +69,7 @@ function queueNextDraw() {
 }
 
 function draw() {
+  g.reset().clearRect(0,24,g.getWidth(),g.getHeight()-IMAGEHEIGHT);
   var locale = require("locale");
 
   var date = new Date();
