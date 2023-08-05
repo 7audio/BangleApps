75f52c1e143e1b963ababc39f35aa4e1ee5e2889
Tikhon Sokolov <tikhon.sokolov>
2023-08-05 18:31:41 +0700

    bigger font

--- apps/widklok/widget.js
+++ apps/widklok/widget.js
@@ -1,7 +1,7 @@
 WIDGETS["widklok"] = { area:"tr", width:16, draw: function() {
   var date = new Date();
   var timeStr = require("locale").time(date, 1).split(":"); // Hour and minute
-  g.reset().setColor(g.theme.dark ? "#0ff" : "#00f").setFont("6x8", 1);
+  g.reset().setColor(g.theme.dark ? "#0ff" : "#00f").setFont("Vector", 11);
   g.drawString(timeStr[0], this.x+2, this.y+4, true);
   g.drawString(timeStr[1], this.x+2, this.y+14, true);
   if (this.drawTimeout) clearTimeout(this.drawTimeout);
