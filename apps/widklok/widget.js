c39af76de818bf9eee3b8ae39b34aeee9c9e3c7e
Tikhon Sokolov <tikhon.sokolov>
2024-12-28 13:04:18 +0700

    bigger digits in clock widget

--- apps/widklok/widget.js
+++ apps/widklok/widget.js
@@ -1,8 +1,8 @@
-WIDGETS["widklok"] = { area:"tr", width: 56, draw: function() {
+WIDGETS["widklok"] = { area:"tr", width: 48, draw: function() {
   var date = new Date();
   var timeStr = require("locale").time(date, 1);
   g.reset().setColor(g.theme.dark ? "#fff" : "#000").setFont("Vector", 16);
-  g.drawString(timeStr, this.x+2, this.y+2, true);
+  g.drawString(timeStr, this.x+2, this.y+6, true);
   if (this.drawTimeout) clearTimeout(this.drawTimeout);
   this.drawTimeout = setTimeout(()=>{
     this.drawTimeout = undefined;
